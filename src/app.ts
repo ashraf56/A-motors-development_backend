0d7b122bc28d8b83d4c2b7910aa6763a01bf6ae6
ashraf56 <ashrafulfahim07@gmail.com>
2024-09-17 16:34:46 +0600

    code modified

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import globalErrorhandler from './app/middleware/globalErrorhandler';
 
 const app = express()
 
-app.use(cors({origin:['http://localhost:5173'], credentials:true}))
+app.use(cors({origin:['https://a-motors-development-backend.vercel.app/'], credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api', router)
